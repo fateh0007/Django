9649523cefeb592b89d5b4d56e7cbf369872fc06
fateh0007 <fatehalam123463@gmail.com>
2025-12-06 11:48:02 +0530

    Added Middleware for browser reload

--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "chaiaurDjango.urls"
